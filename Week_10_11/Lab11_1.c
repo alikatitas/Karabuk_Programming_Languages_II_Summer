82473038b6c292afa9bc252ee6f851126409c3f9
alikatitas <alikatitas@e-mail.com.tr>
2022-09-06 01:27:06 +0300

    working

--- Week_10_11/Lab11_1.c
+++ Week_10_11/Lab11_1.c
@@ -6,85 +6,73 @@ dosya olusturan ve bu dosya içerisinde isme göre arama yapan programi
 yaziniz. Program menü seklinde hazirlanmalidir. Dosya olusturma, kayit
 ekleme gibi gerekli tüm fonksiyonlar yazilmalidir.*/
 
-struct student{
+//dosya yazma
+void dosyaYaz(){
 	int no;
 	char name[20];
 	char departmentName[20];
-	struct student *next;
-};
-
-typedef struct student std;
-std *head, *prevNode;
-
-int kayitAra(char *aranan){
-	std *p;
-	p=head;
-	
-	prevNode=head;
-	while(p->next==NULL){
-		p=p->next;
-		if(strcmp(p->name == aranan) >= 0) break;
-		prevNode=p;	
-		}
-	
-}
-
-void yeniKayit(std *newStudent){
-	std *p;
-	p=head;
-	int i,ogrSayisi;
-	FILE *file =fopen("students.txt","w");
-	
-	if(file==NULL){
+	//dosyaya yazma
+	FILE *file;
+	if((file=fopen("students.dat","w"))==NULL){
 		printf("Dosya olusturulamadi");
 	}else{
-	printf("Kayit Edilecek Ogrenci Sayisini Girin : ");
-	scanf("%d",&ogrSayisi);
-	
-	printf("Ogrenci Bilgilerini Girin :\n");
-	
-	for(i=0;i<ogrSayisi;i++){
-		printf("%d. Ogrencinin Numarasi : ",i+1);
-		scanf("%d",&newStudent->no);
-		printf("%d. Ogrencinin Ismi : ",i+1);
-		scanf("%s",newStudent->name);
-		printf("%d. Ogrencinin Departmani : ",i+1);
-		scanf("%s",newStudent->departmentName);	
+		printf("Ogrenci No, Isim ve Bolum bilgilerini girin : \n");
+		printf("Veri girisini bitirmek icin 0 girin : \n");
+		scanf("%d%s%s",&no,name,departmentName);
+		
+		while(no != 0 ){
+			fprintf(file,"%d%s%s\n",no,name,departmentName);
+			scanf("%d%s%s",&no,name,departmentName);
+		}
+		printf("Dosya olusturma islemi tamamlandi..");
 	}
-	
-		while(!feof(file)){
-		p=p->next;
-		p->next=newStudent;
-		fprintf(file,"%d %s %s\n",&(newStudent+i)->no, (newStudent+i)->name, (newStudent+i)->departmentName);
-	}
-	
-}
-	fclose(file);
 }
 
+//kayit ara
+void ara(char *aranan){
+	int no;
+	char name[20];
+	char departmentName[20];
+	FILE *file;
+	if((file=fopen("students.dat","r"))==NULL){
+		printf("Dosya acilamadi");
+	}else{
+		fscanf(file,"%d%s%s",&no,name,departmentName);
+		if(no == aranan)
+		printf("Kayit bulundu");
+		else
+		printf("Kayit bulunamadi");
+		}
+	}
+
+//menu
 void menu(){
-	printf("Ogrenci Bilgileri :\n" 
-	"1-Dosya Olustur ve Kayit Ekle\n"
-	"2-Ogrenci Isminden Kayit Ara\n");
+	printf("Ogrenci Programi\n"
+	"1-Kayit Ekle\n"
+	"2-Ogrenci Noya gore Arama Yap\n"
+	"3-Cikis");
 }
 
 int main(void){
-	
-	int secim;
 	menu();
-	printf("Seciminizi Yapin :");
+	int secim;
+	printf("\nSeciminizi Yapin : ");
 	scanf("%d",&secim);
-	
-	std *newStd;
-	newStd=(std *)malloc(sizeof(std));
-	
-		switch(secim){
+	switch(secim){
 		case 1:
-			yeniKayit(&newStd);
+			dosyaYaz();
 			break;
 		case 2:
-			kayitAra(head);
+			printf("Aradiginiz ogrencininin numarasini yazin : ");
+			int stdNo;
+			scanf("%d",&stdNo);
+			ara(stdNo);
+			break;
+		case 3:
+			printf("Programdan cikildi..");
+			exit(0);
 			break;
 	}
+	
 	return 0;
 }
